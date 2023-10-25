1c88c7e7bf2922ffd2da4bda9ecad3254aa51b98
Jhony Souza <jhonatansouzameza100@gmail.com>
2023-10-25 01:35:59 -0300

    first commit

---
+++ src/data.js
@@ -0,0 +1,26 @@
+export const tasks = [
+  {
+    id: 1,
+    title: "Task 1",
+    description: "This is task 1",
+    priority: "hight",
+    author: "jhon Doe",
+    createdAt: "24/12/2015"
+  },
+  {
+    id: 2,
+    title: "Task 2",
+    description: "This is task 2",
+    priority: "medium",
+    author: "Chupete",
+    createdAt: "18/12/2018"
+  },
+  {
+    id: 3,
+    title: "Task 3",
+    description: "This is task 3",
+    priority: "low",
+    author: "???",
+    createdAt: "23/12/24"
+  }
+]
